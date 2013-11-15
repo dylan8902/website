0e9032ee82ac62858b28226e78dfedf04c6a7a2e
Dylan Jones <dyl@anjon.es>
2013-11-15 08:22:36 +0000

    adding links and icons to /stream

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -5,17 +5,45 @@ class StreamController < ApplicationController
   def index
     Project.hit 16
     @stream = []
+
     Project.all.each do |project|
-      @stream << { title: project.title, description: project.description, created_at: project.created_at }
+      @stream << {
+        title: project.title,
+        description: project.description,
+        icon: :code,
+        created_at: project.created_at,
+        link: project.url
+      }
     end
+
     BlogPost.all.each do |post|
-      @stream << { title: post.title, description: "I wrote a blog", created_at: post.created_at }
+      @stream << {
+        title: post.title,
+        description: post.text.to(400).html_safe + "...",
+        icon: :edit,
+        created_at: post.created_at,
+        link: blog_post_path(post)
+      }
     end
+
     Tweet.all.each do |tweet|
-      @stream << { title: "Tweet", description: tweet.text, created_at: tweet.created_at }
+      @stream << {
+        title: "Tweet",
+        description: tweet.text,
+        icon: "twitter",
+        created_at: tweet.created_at,
+        link: tweet_path(tweet)
+      }
     end
+
     FacebookPost.all.each do |post|
-      @stream << { title: "Facebook", description: post.text, created_at: post.created_at }
+      @stream << {
+        title: "Facebook",
+        description: post.text,
+        icon: "facebook",
+        created_at: post.created_at,
+        link: facebook_post_path(post)
+      }
     end
 
     @stream.sort_by! do |post|
