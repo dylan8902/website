23434060a533e96bfa6b394772ee390e13ac5192
Dylan Jones <dyl@anjon.es>
2013-11-27 07:39:48 +0000

    additions to cleversounds
    layout changes but still need images and video

--- app/controllers/cleversounds_controller.rb
+++ app/controllers/cleversounds_controller.rb
@@ -5,7 +5,7 @@ class CleversoundsController < ApplicationController
   # GET /cleversounds.json
   # GET /cleversounds.xml
   def index
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: nil, callback: params[:callback] }
