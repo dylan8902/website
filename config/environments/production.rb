e9a9f09aa46832efa080df3bf40872011915e541
Dylan Jones <dyl@anjon.es>
2013-12-08 10:54:27 +0000

    removes old file fixes a link in who and ensures assets are compressed in compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Website::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
+  config.assets.compress = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
