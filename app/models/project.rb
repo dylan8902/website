cd83d504afcf941c2bf110ae7848726b7730aa9f
Dylan Jones <dyl@anjon.es>
2014-07-06 09:14:02 +0100

    fixes #86 adds default scope for stuff

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :url, presence: true
-
+  default_scope { order('created_at DESC') }
 
   def self.hit id
     project = Project.where(id: id).first
