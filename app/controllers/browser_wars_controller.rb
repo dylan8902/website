7bdd618fdf3e6ceec1e6193843a98c2a66b0bf47
Dylan Jones <dyl@anjon.es>
2014-11-24 22:25:18 +0000

    fixes #94. twitter gem deprecated stuff, now fixed

--- app/controllers/browser_wars_controller.rb
+++ app/controllers/browser_wars_controller.rb
@@ -12,21 +12,20 @@ class BrowserWarsController < ApplicationController
       @browsers = JSON.load filename
     else
 
-      Twitter.configure do |config|
-        config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-        config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-        config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
-        config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token        = ENV['TWITTER_OAUTH_TOKEN']
+        config.access_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
       end
-      browsers = Twitter.users('opera', 'firefox', 'ie', 'googlechrome', 'AppleSafari')
-      @bowsers = browsers.to_json
-      
+      browsers = client.users('opera', 'firefox', 'ie', 'googlechrome', 'AppleSafari')
       File.open(filename, "w") do |f|
-        f.write(@bowsers)
+        f.write(browsers.to_json)
       end
-      
+      @browsers = JSON.load filename
+
     end
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @browsers, callback: params[:callback] }
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,15 +2,15 @@ class Tweet < ActiveRecord::Base
   default_scope { order('created_at DESC') }
 
   def self.update
-    
-    Twitter.configure do |config|
-      config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-      config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
-      config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = ENV['TWITTER_OAUTH_TOKEN']
+      config.access_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
     end
     
-    tweets = Twitter.user_timeline("dylan8902")
+    tweets = client.user_timeline("dylan8902")
     tweets.each do |tweet|
       unless tweet.geo.nil?
         lat = tweet.geo.coordinates[0]
