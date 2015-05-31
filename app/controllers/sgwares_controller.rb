07454aceb3b4f2343e5b7a1a913bcc5b50510a48
Dylan Jones <dyl@anjon.es>
2015-05-31 22:44:39 +0100

    move to standard layout

--- app/controllers/sgwares_controller.rb
+++ app/controllers/sgwares_controller.rb
@@ -1,15 +1,15 @@
 class SgwaresController < ApplicationController
-  layout false
-  
+
+
   # GET /sgwares
   def index
     #Project.hit 23
   end
- 
+
    
-  # GET /sgwares/facebook
+  # POST /sgwares/facebook
   def facebook
-    #Project.hit 23
+    render(layout: false)
   end
 
 
