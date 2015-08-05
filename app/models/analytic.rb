e194f31ec917bd8523db688998d3c6812f0407e5
Dylan Jones <dyl@anjon.es>
2015-08-05 19:26:01 +0100

    add fix for referees that are very big and can cause a db problem

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,11 +1,27 @@
 class Analytic < ActiveRecord::Base
 
   def self.hit request
+  	referer = nil
+  	if request.referer
+      referer = request.referer.truncate(255)
+    end
+    user_agent = nil
+    if request.user_agent
+      user_agent = request.user_agent.truncate(255)
+    end
+    remote_ip = nil
+    if request.remote_ip
+      remote_ip = request.remote_ip.truncate(255)
+    end
+    original_url = nil
+    if request.original_url
+      original_url = request.original_url.truncate(255)
+    end
      Analytic.create(
-       uri: request.original_url,
-       ip: request.remote_ip,
-       user_agent: request.user_agent,
-       referer: request.referer
+       uri: original_url,
+       ip: remote_ip,
+       user_agent: user_agent,
+       referer: referer
      )
   end
 
