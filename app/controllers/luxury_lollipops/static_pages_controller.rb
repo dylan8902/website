c790cfb747f10b6fdc1ce3b01f750aa19655ea4a
Dylan Jones <dyl@anjon.es>
2016-12-29 23:21:04 +0000

    Update whole template and add 2 views

--- app/controllers/luxury_lollipops/static_pages_controller.rb
+++ app/controllers/luxury_lollipops/static_pages_controller.rb
@@ -2,11 +2,17 @@ class LuxuryLollipops::StaticPagesController < ApplicationController
   layout "luxury_lollipops"
   after_filter :analytics
 
-
   # GET /
   def index
   end
 
+  # GET /make-a-request
+  def request
+  end
+
+  # GET /take-a-look
+  def photos
+  end
 
   private
     def analytics
