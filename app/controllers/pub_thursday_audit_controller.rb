e2baf4bbdb6b4c8ce2ff4bf18115217d34e7fb87
Dylan Jones <dyl@anjon.es>
2024-02-20 06:16:14 +0000

    Add pics, links and cover all overlap scenarios

--- app/controllers/pub_thursday_audit_controller.rb
+++ app/controllers/pub_thursday_audit_controller.rb
@@ -9,18 +9,22 @@ class PubThursdayAuditController < ApplicationController
   # GET /pub-thursday-audit.xml
   def index
 
-    @users = {}
     project = "pub-tracker-live"
-    base_url = "https://firestore.googleapis.com/v1/projects/#{project}/databases/(default)/documents"
-    response = JSON.parse(RestClient.get("#{base_url}/users?mask.fieldPaths=displayName&pageSize=300").body)
+    api_url = "https://firestore.googleapis.com/v1/"
+    base_url = "#{api_url}projects/#{project}/databases/(default)/documents"
+
+    @users = {}
+
+    response = JSON.parse(RestClient.get("#{base_url}/users?mask.fieldPaths=displayName&mask.fieldPaths=photoURL&pageSize=300").body)
     response["documents"].each do |user|
       display_name = user["fields"]["displayName"]["stringValue"]
-      @users[user["name"]] = { name: display_name, sessions: [] }
+      photo_url = user["fields"]["photoURL"]["stringValue"]
+      @users[user["name"]] = { name: display_name, photo: photo_url, sessions: [] }
     end
 
     documents = []
 
-    url = "#{base_url}/sessions?orderBy=startTime%20desc&mask.fieldPaths=startTime&mask.fieldPaths=endTime&mask.fieldPaths=userRef&mask.fieldPaths=locationRef&pageSize=300"
+    url = "#{base_url}/sessions?orderBy=startTime%20desc&mask.fieldPaths=startTime&mask.fieldPaths=endTime&mask.fieldPaths=userRef&mask.fieldPaths=locationName&pageSize=300"
     response = JSON.parse(RestClient.get(url).body)
     documents.concat response["documents"]
 
@@ -32,7 +36,14 @@ class PubThursdayAuditController < ApplicationController
       ref = session["fields"]["userRef"]["referenceValue"]
       start_time = session["fields"]["startTime"]["timestampValue"]
       end_time = session["fields"]["endTime"]["timestampValue"]
-      @users[ref][:sessions] << { id: session["name"], start: DateTime.parse(start_time), end: DateTime.parse(end_time) }
+      location = session["fields"]["locationName"]["stringValue"]
+      @users[ref][:sessions] << {
+        id: session["name"],
+        url: "#{api_url}#{session["name"]}",
+        start: DateTime.parse(start_time),
+        end: DateTime.parse(end_time),
+        location: location
+      }
     end
 
     @users.delete_if do |k,v|
@@ -42,11 +53,28 @@ class PubThursdayAuditController < ApplicationController
     @users.each do |key, user|
       user[:sessions].each do |session|
         user[:sessions].each do |other_session|
-          if other_session[:start] > session[:start] and other_session[:end] < session[:end]
-            session[:within] = other_session
+          if
+            (other_session[:start] > session[:start] and other_session[:end] < session[:end]) ||
+            (other_session[:start] < session[:start] and other_session[:end] > session[:end]) ||
+            (other_session[:start] > session[:start] and other_session[:start] < session[:end] and other_session[:end] > session[:end]) ||
+            (other_session[:start] < session[:start] and other_session[:end] < session[:end] and other_session[:end] > session[:start])
+            session[:within] = {
+              id: other_session[:id],
+              url: other_session[:url],
+              start: other_session[:start],
+              end: other_session[:end]
+            }
+            user[:illegal] = true
           end
         end
       end
+      user[:sessions].delete_if do |session|
+        session[:within].nil?
+      end
+    end
+
+    @users.delete_if do |k,v|
+      v[:illegal].nil?
     end
 
     respond_to do |format|
