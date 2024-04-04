2a227704acc5853ecc00f84a8aa4cf1eaaefaa22
Dylan Jones <dyl@anjon.es>
2024-04-04 01:32:49 +0100

    Allows users to amend broken sessions

--- app/controllers/pub_thursday_audit_controller.rb
+++ app/controllers/pub_thursday_audit_controller.rb
@@ -2,6 +2,10 @@ class PubThursdayAuditController < ApplicationController
   include ErrorHelper
 
   FILENAME = Rails.root.join("json", "pub-thursday-audit.json")
+  PROJECT = "pub-tracker-live"
+  API_URL = "https://firestore.googleapis.com/v1/"
+  DOCUMENT_ROOT = "projects/#{PROJECT}/databases/(default)/documents"
+  BASE_URL = "#{API_URL}#{DOCUMENT_ROOT}"
 
 
   # GET /pub-thursday-audit
@@ -11,22 +15,18 @@ class PubThursdayAuditController < ApplicationController
     if File.exist?(FILENAME) and params[:refresh].nil?
       @users = load_users
     else
-      project = "pub-tracker-live"
-      api_url = "https://firestore.googleapis.com/v1/"
-      base_url = "#{api_url}projects/#{project}/databases/(default)/documents"
-
       @users = {}
 
-      response = JSON.parse(RestClient.get("#{base_url}/users?mask.fieldPaths=displayName&mask.fieldPaths=photoURL&pageSize=300").body)
+      response = JSON.parse(RestClient.get("#{BASE_URL}/users?mask.fieldPaths=displayName&mask.fieldPaths=photoURL&pageSize=300").body)
       response["documents"].each do |user|
         display_name = user["fields"]["displayName"]["stringValue"]
         photo_url = user["fields"]["photoURL"]["stringValue"]
-        @users[user["name"]] = { name: display_name, photo: photo_url, sessions: [] }
+        @users[user["name"]] = { id: user["name"].split("/").last, name: display_name, photo: photo_url, sessions: [] }
       end
 
       documents = []
 
-      url = "#{base_url}/sessions?orderBy=startTime%20desc&mask.fieldPaths=startTime&mask.fieldPaths=endTime&mask.fieldPaths=userRef&mask.fieldPaths=locationName&pageSize=300"
+      url = "#{BASE_URL}/sessions?orderBy=startTime%20desc&mask.fieldPaths=startTime&mask.fieldPaths=endTime&mask.fieldPaths=userRef&mask.fieldPaths=locationName&mask.fieldPaths=time&pageSize=300"
       response = JSON.parse(RestClient.get(url).body)
       documents.concat response["documents"]
 
@@ -37,15 +37,18 @@ class PubThursdayAuditController < ApplicationController
       end
 
       documents.each do |session|
+        logger.info session
         ref = session["fields"]["userRef"]["referenceValue"]
         start_time = DateTime.parse(session["fields"]["startTime"]["timestampValue"])
         end_time = DateTime.parse(session["fields"]["endTime"]["timestampValue"])
+        time = (session["fields"]["time"] || {} )["doubleValue"]
         location = session["fields"]["locationName"]["stringValue"]
         @users[ref][:sessions] << {
           id: session["name"].split('/').last,
-          url: "#{api_url}#{session["name"]}",
+          url: "#{API_URL}#{session["name"]}",
           start: start_time,
           end: end_time,
+          time: time,
           duration: TimeDifference.between(start_time, end_time).humanize,
           location: location
         }
@@ -67,6 +70,7 @@ class PubThursdayAuditController < ApplicationController
               (other_session[:start] < session[:start] and other_session[:end] < session[:end] and other_session[:end] > session[:start])
               session[:within] = Marshal.load(Marshal.dump(other_session))
               session[:within][:longer] = (other_session[:end] - other_session[:start]) > (session[:end] - session[:start])
+              session[:fixed] = session[:time] == 0
               user[:illegal] = true
             end
           end
@@ -114,6 +118,7 @@ class PubThursdayAuditController < ApplicationController
   # GET /pub-thursday-audit/session-id.xml
   def show
     @session = find_session params[:id]
+    @timestamps = timestamps @session
 
     respond_to do |format|
       format.html # session.html.erb
@@ -128,6 +133,48 @@ class PubThursdayAuditController < ApplicationController
   # POST /pub-thursday-audit/session-id.xml
   def delete
     @session = find_session params[:id]
+    @timestamps = timestamps @session
+
+    @response = JSON.parse(RestClient.get("#{BASE_URL}/sessions/#{@session[:id]}").body)
+
+    if (@response["fields"]["time"] || {})["doubleValue"] != 0
+      field_transforms = []
+      @timestamps.each do |k, v|
+        field_transforms.push({
+          increment: {
+            "#{v.instance_of?(Integer) ? "integerValue" : "doubleValue"}": v
+          },
+          fieldPath: k
+        })
+      end
+
+      transform = {
+        writes: [
+          {
+            transform: {
+              document: "#{DOCUMENT_ROOT}/users/#{@session[:user][:id]}",
+              fieldTransforms: field_transforms
+            }
+          }
+        ]
+      }
+      url = "#{BASE_URL}:commit"
+      logger.info "Transforming user #{@session[:user]} - #{url} with #{transform.to_json}"
+      RestClient.post(url, transform.to_json, content_type: :json)
+
+      zero_time = {
+        fields: {
+          time: {
+            doubleValue: 0
+          }
+        }
+      }
+      url = "#{BASE_URL}/sessions/#{@session[:id]}?updateMask.fieldPaths=time&currentDocument.exists=true"
+      logger.info "Zero-ing session time #{@session[:id]} - #{url} with #{zero_time.to_json}"
+      RestClient.patch(url, zero_time.to_json, content_type: :json)
+    else
+      logger.info "Session already fixed #{@session}"
+    end
 
     respond_to do |format|
       format.html # delete.html.erb
@@ -158,12 +205,26 @@ class PubThursdayAuditController < ApplicationController
     load_users.each do |user|
       user[:sessions].each do |session|
         if session[:id] == id
+          session[:user] = user.except(:sessions)
           return session
         end
       end
     end
 
     render_404
+  end
+
+  def timestamps session
+    ms = -(session[:end].strftime("%Q").to_i - session[:start].strftime("%Q").to_i)
+    seconds = ms / 1000.to_f
+    return {
+      "#{session[:start].strftime('T%Y')}": ms,
+      "#{session[:start].strftime('T%YM%m')}": ms,
+      "#{session[:start].strftime('T%YW%V')}": ms,
+      "Ttotal": ms,
+      "yearTime": seconds,
+      "totalTime": seconds
+    }
   end
 
 end
